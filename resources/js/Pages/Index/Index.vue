5ca25b680ff8b1c8538ecea9e1712d850565823c
mariannehartigan <mariannehartigan@yahoo.ca>
2024-09-25 18:35:05 -0400

    Layouts and Props

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,7 +1,12 @@
-<template>Hello {{ counter }}!</template>
+<template>
+  <div>Index</div>
+  <Link href="/hello">Show Page</Link>
+  <div>
+    The message is {{ message }}
+  </div>
+</template>
 
 <script setup>
-  import {ref} from 'vue'
-  const counter = ref(0)
-  setInterval(() => counter.value++, 1000)
+import { Link } from '@inertiajs/vue3'
+defineProps({ message: String })
 </script>
